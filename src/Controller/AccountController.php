1b82b0d8a0920b58a578df528478dfb20aad6eda
YazidElg <yazidelg5@gmail.com>
2020-06-13 20:25:30 +0200

    commit password

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -7,9 +7,11 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\User;
+use App\Form\PasswordUpdateType;
+use App\Entity\PasswordUpdate;
 use  App\Form\AccountType;
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Form\RegistrationType;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -52,16 +54,15 @@ class AccountController extends AbstractController
      */
 
 
-    public function register(Request $request /*, UserPasswordEncoderInterface $encoder*/ ) {
-        $user = new User();
-        
+    public function register(Request $request,  UserPasswordEncoderInterface $encoder){
+        $user = new User();        
         $form = $this->createForm(RegistrationType::class, $user);
 
         $form->handleRequest($request);
        
         if($form->isSubmitted() && $form->isValid()) {
-            //$hash = $encoder->encodePassword($user , $user->getHash());
-            //$user->setHash($hash);
+            $hash = $encoder->encodePassword($user , $user->getHash());
+            $user->setHash($hash);
             $manager = $this->getDoctrine()->getManager();
             $manager->persist($user);
             $manager->flush();
@@ -123,5 +124,46 @@ class AccountController extends AbstractController
             'user' => $this->getUser()
         ]);
     }
-    
+    /**
+     * Permet de modifier le mot de passe
+     * 
+     * @Route("/account/password-update", name="account_password")
+     * 
+     * @return Response
+     */
+    public function updatePassword(Request $request, UserPasswordEncoderInterface $encoder){
+        $passwordUpdate = new PasswordUpdate();
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(PasswordUpdateType::class, $passwordUpdate);
+         
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            // 1 verifier que le old password est le meme que le password du user
+           if(!password_verify($passwordUpdate->getOldPassword(), $user->getHash() )){
+
+           }else{
+                $newPassword = $passwordUpdate->getNewPassword();
+                $hash = $encoder->encodePassword($user, $newPassword);
+                $user->setHash($hash);
+
+                $manager->persist($hash);
+                $manager->flush();
+
+                $this->addFlash(
+                    'success',
+                    "Votre mot de passe a bien été modifié !"
+                );
+                return $this->redirectToRoute('homepage');
+            }
+        
+        }
+
+        return $this->render('account/password.html.twig',[
+            'form' => $form->createView()
+        ]);
+
+    }
 }
